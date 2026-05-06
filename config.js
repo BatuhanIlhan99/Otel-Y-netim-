39b011c59433723ff325cf3e56072d8f1eaf0b6f
BatuhanIlhan99 <270341779+BatuhanIlhan99@users.noreply.github.com>
2026-05-06 22:58:39 +0300

    Deploy otel yonetim uygulamasi

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
-window.OTEL_CONFIG = {
+﻿window.OTEL_CONFIG = {
   apiBaseUrl: "",
   firebaseAppId: "otel-yonetim",
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
+    apiKey: "AIzaSyBPPBq9X8OnlIQRIyZ2Is4ny6VPAphI058",
+    authDomain: "otel-yonetim-656d9.firebaseapp.com",
+    projectId: "otel-yonetim-656d9",
+    storageBucket: "otel-yonetim-656d9.firebasestorage.app",
+    messagingSenderId: "679035392731",
+    appId: "1:679035392731:web:c4d232dad8d02ca64d72fe",
+    measurementId: "G-T24QP6XFTD",
   },
 };
